c7aeb534fd7f29d329090b5e8be1d07457952732
Ishaque <ishaqueahmed.mev@iflair.com>
2019-10-15 12:23:50 +0530

    Routes changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/api/home', (req, res) => {
   res.send({ message: 'YOU COME AT THE KING, BEST NOT MISS' })
 })
 
