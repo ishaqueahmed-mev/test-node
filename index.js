ff460de32061587b61fd9e0a9f7d76fded9cee1f
Ishaque <ishaqueahmed.mev@iflair.com>
2019-10-15 12:10:03 +0530

    Changes

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('YOU COME AT THE KING, BEST NOT MISS')
+  res.send({ message: 'YOU COME AT THE KING, BEST NOT MISS' })
 })
 
-app.listen(7200);
+app.listen(7200, () => {
+  console.log('Up and running')
+});
